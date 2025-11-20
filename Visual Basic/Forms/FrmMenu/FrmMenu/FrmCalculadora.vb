7f067b1641feecf124d0e17854cf781883694dd5
donivanovich <kosoivan12@gmail.com>
2025-11-20 18:57:20 +0100

    Subida de Archivos

---
+++ Visual Basic/Forms/FrmMenu/FrmMenu/FrmCalculadora.vb
@@ -0,0 +1,31 @@
+﻿Public Class FrmCalculadora
+
+    Dim Valor As Integer = 10
+
+    Private Sub FrmCalculadora_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+
+        Dim Res As Byte ' Esto es la declaración de una variable local
+        Res = MessageBox.Show("¿Desea Finalizar?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If Res = vbNo Then e.Cancel = True
+
+
+        'Else
+        '    If Res = vbYes Then
+        '    Else
+        '        If Res = vbYes Then
+
+
+        '        End If
+
+
+        '    End If
+        'End If
+
+
+
+    End Sub
+End Class
+
+
+
+
