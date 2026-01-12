37e97d95ae95ceaec2b4a9680d4bd3e3126d9889
donivanovich <kosoivan12@gmail.com>
2026-01-12 21:10:34 +0100

    kt

---
+++ kt/app/src/main/java/com/ikf/listarcontactos/MainActivity.kt
@@ -0,0 +1,58 @@
+    package com.ikf.listarcontactos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ikf.listarcontactos.ui.theme.ListarContactosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ListarContactosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    NameView(modifier = Modifier.padding(paddingValues = innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NameView(modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier
+    ) {
+        Text(text = "Hola Clase")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ListarContactosTheme {
+        Greeting("Android")
+    }
+}
